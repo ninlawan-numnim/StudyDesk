dcebd784e629794d4d4b5d771e1cce8aa6fb7d0a
ninlawan <ninlawan2212@gmail.com>
2026-08-21 22:23:30 +0700

    fix : syntax of main.ts

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,7 @@
 import { createApp } from 'vue'
 import './assets/styles/base.css'
 import App from './App.vue'
-import { saveSession, loadSession, closeDb, getSessionByPdfPath, createSession, insertPomodoroLog, saveQuiz, type SaveQuizInput } from './electron/database 
-'
+
 
 import * as pdfjsLib from 'pdfjs-dist'
 
